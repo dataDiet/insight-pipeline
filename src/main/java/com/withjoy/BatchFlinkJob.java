1011b31845398d65ae3188e6087ff85061319d64
dataDiet <akiramadono@gmail.com>
2017-06-16 13:21:45 -0700

    memory as an enclosure

--- src/main/java/com/withjoy/BatchFlinkJob.java
+++ src/main/java/com/withjoy/BatchFlinkJob.java
@@ -31,36 +31,102 @@ import org.apache.flink.core.fs.FileSystem.WriteMode;
 
 public class BatchFlinkJob {
         
-    private static HashMap<String, Integer> activity_columns_string_int = new HashMap<String, Integer>();
-    private static HashMap<String, Integer> tracks_columns_string_int = new HashMap<String, Integer>();
-    private static HashMap<Integer, String> activity_columns_int_string = new HashMap<Integer, String>();
-    private static HashMap<Integer, String> tracks_columns_int_string = new HashMap<Integer, String>();
-
-    public static void main(String[] args){
-        
+    private static HashMap<String, Integer> activity_string_int = new HashMap<String, Integer>();
+    private static HashMap<String, Integer> tracks_string_int = new HashMap<String, Integer>();
+    
+    public static void invertHashMap(HashMap<String,Integer> input_hash, HashMap<Integer, String> output_hash){
+        Iterator<Entry<String,Integer>> iterator = input_hash.entrySet().iterator();
+        while(iterator.hasNext()){
+            String table_name=iterator.next().getKey();
+            output_hash.put(input_hash.get(table_name), table_name);
+        }
+    }
+    public BatchFlinkJob(ParameterTool params){
+     
+       // set up the batch execution environment        
         //Determine relevant columns and their orderings for use in map
-        Properties sql_properties = new AcquireProperties("properties.txt").getProperties();
-        String schema = sql_properties.getProperty("pg_schema");
-
-        // set up the batch execution environment
-        final ParameterTool params = ParameterTool.fromArgs(args);
-
-        activity_columns_string_int = ReadPostgreSQL.getSQLHash("SELECT * FROM "+schema+"."+params.get("input")+" LIMIT 1");
-        tracks_columns_string_int = ReadPostgreSQL.getSQLHash("SELECT * FROM "+schema+"."+"tracks"+" LIMIT 1");
+        Properties properties = new AcquireProperties("properties.txt").getProperties();
+        String schema = properties.getProperty("pg_schema");
+        String aws_bucket_in = properties.getProperty("aws_bucket_in");
+        String aws_bucket_out = properties.getProperty("aws_bucket_out");
         
-        invertHashMap(activity_columns_string_int,activity_columns_int_string);
-        invertHashMap(tracks_columns_string_int,tracks_columns_int_string);
+        final HashMap<String, Integer> activity_string_int = ReadPostgreSQL.getSQLHash("SELECT * FROM "+schema+"."+params.get("input")+" LIMIT 1");
+        final HashMap<String, Integer> tracks_string_int = ReadPostgreSQL.getSQLHash("SELECT * FROM "+schema+"."+"tracks"+" LIMIT 1");
+        final HashMap<Integer, String> activity_int_string = new HashMap<Integer, String>();
+        final HashMap<Integer, String> tracks_int_string = new HashMap<Integer, String>();
+ 
+        invertHashMap(activity_string_int,activity_int_string);
+        invertHashMap(tracks_string_int,tracks_int_string);
         
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-
-        DataSet<String> text = env.readTextFile(params.get("input"));
         
-        //DataSet<String> text = env.readTextFile(event);
-        DataSet<String> text_with_json = text.map(new Parser());
+        DataSet<String> text = env.readTextFile("s3a://"+aws_bucket_in+"/"+params.get("input"));
+
+        DataSet<String> text_with_json = text.map(new MapFunction<String, String>(){
+            public String map(String tsv_input){
+                int tracks_num_columns=tracks_int_string.size();
+                int activity_num_columns=activity_int_string.size();
+                List<String> tsv_row=new ArrayList<>(tracks_num_columns);
+                String output_left=null;
+                JSONObject output_right =new JSONObject();
+
+                try{
+                    CSVRecord tsv_records = CSVParser.parse(tsv_input, CSVFormat.TDF).getRecords().get(0);                
+                    int i=1;
+
+                    for(;i <= tracks_num_columns;i++){
+                        String tracks_column= tracks_int_string.get((Integer)i);
+                        if(activity_string_int.containsKey(tracks_column)){
+                            String string_data=tsv_records.
+                                    get(activity_string_int.
+                                    get(tracks_column)-1);
+                            if(string_data.equals("\\N")){
+                                tsv_row.add("NULL");
+                            }
+                            else{
+                                tsv_row.add(string_data);
+                            }
+                        }
+                        else{
+                            tsv_row.add("NULL");
+                        }
+                    }
+                    output_left = String.join("|", tsv_row);
+                    for(i=1;i <= activity_num_columns;i++){
+                        String activity_column = activity_int_string.get((Integer)i);
+                        if(!tracks_string_int.containsKey(activity_column)){
+                            String string_data_json=tsv_records.
+                                    get(activity_string_int.
+                                    get(activity_column)-1);
+                            if(string_data_json.equals("\\N")){
+                                output_right.put(activity_column,"NULL");
+                            }
+                            else{
+                                output_right.put(activity_column,string_data_json);  
+                            }
+                        }
+                    }
+                    return output_left+'|'+output_right.toString();
+                }
+                catch(IOException e){
+                    System.err.println("Problem reading record: "+tsv_input);
+                }
+                catch(JSONException e){
+                    System.err.println("Problem making json: "+tsv_input);
+                }
+                activity_string_int.clear();
+                tracks_string_int.clear();
+                activity_int_string.clear();
+                tracks_int_string.clear();
+                return "NULL";
+            }
+        });
+        
+
         try{
             if (params.has("output")) {
-                String aws_bucket = new AcquireProperties("properties.txt").getProperties().getProperty("aws_bucket");
-                text_with_json.writeAsText("s3a://"+aws_bucket+"/"+params.get("output"),WriteMode.OVERWRITE);
+                text_with_json.writeAsText("s3a://"+aws_bucket_out+"/"+params.get("output"),WriteMode.OVERWRITE);
+
                 // execute program
                 env.execute("Wrote text file output");
             } else {
@@ -70,77 +136,9 @@ public class BatchFlinkJob {
         }
         catch(Exception e){
             System.out.println("Flink Map Exception");
-        }
-        activity_columns_string_int.clear();
-        tracks_columns_string_int.clear();
-        activity_columns_int_string.clear();
-        tracks_columns_int_string.clear();
-        
+        }        
     }
     
-    public static void invertHashMap(HashMap<String,Integer> input_hash, HashMap<Integer, String> output_hash){
-        Iterator<Entry<String,Integer>> iterator = input_hash.entrySet().iterator();
-        while(iterator.hasNext()){
-            String table_name=iterator.next().getKey();
-            output_hash.put(input_hash.get(table_name), table_name);
-        }
-    }
-    // User-defined functions
-    public static final class Parser implements MapFunction<String, String> {
-        @Override
-        public String map(String tsv_input) {
-            int tracks_num_columns=tracks_columns_int_string.size();
-            int activity_num_columns=activity_columns_int_string.size();
 
-            List<String> tsv_row=new ArrayList<>(tracks_num_columns);
-            String output_left = null;
-            JSONObject output_right =new JSONObject();
-            
-            try{
-                CSVRecord tsv_records = CSVParser.parse(tsv_input, CSVFormat.TDF).getRecords().get(0);                
-                int i=1;
-                
-                for(;i <= tracks_num_columns;i++){
-                    String tracks_column= tracks_columns_int_string.get((Integer)i);
-                    if(activity_columns_string_int.containsKey(tracks_column)){
-                        String string_data=tsv_records.
-                                get(activity_columns_string_int.
-                                get(tracks_column)-1);
-                        if(string_data.equals("\\N")){
-                            tsv_row.add(" ");
-                        }
-                        else{
-                            tsv_row.add(string_data);
-                        }
-                    }
-                    else{
-                        tsv_row.add(" ");
-                    }
-                }
-                output_left = String.join("\t", tsv_row);
-                for(i=1;i <= activity_num_columns;i++){
-                    String activity_column = activity_columns_int_string.get((Integer)i);
-                    if(!tracks_columns_string_int.containsKey(activity_column)){
-                        String string_data_json=tsv_records.
-                                get(activity_columns_string_int.
-                                get(activity_column)-1);
-                        if(string_data_json.equals("\\N")){
-                            output_right.put(activity_column,"NULL");
-                        }
-                        else{
-                            output_right.put(activity_column,string_data_json);  
-                        }
-                    }
-                }
-                return output_left+'\t'+output_right.toString();
-            }
-            catch(IOException e){
-                System.err.println("Problem reading record: "+tsv_input);
-            }
-            catch(JSONException e){
-                System.err.println("Problem making json: "+tsv_input);
-            }
-            return "";
-        }
-    }
+
 }
